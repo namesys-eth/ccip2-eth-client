fd3fde24ae81d1493acd89e61c90d7f01b48ca58
sshmatrix <cryptosauron@protonmail.com>
2023-04-16 16:05:30 +0530

    gh-pages

---
+++ out/_next/static/pW4QmltxCzHYybjP_efUK/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/b3aa451a-2514e4397a40bd81.js","static/chunks/480-f878c24ec678331b.js","static/chunks/pages/index-2d98e2f17ac73d09.js"],"/_error":["static/chunks/pages/_error-d419484d69bbcf53.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
