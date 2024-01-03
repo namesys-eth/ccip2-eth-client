4e8ea32823ee6f76d431807b03f5fc81a805b9e1
sshmatrix <cryptosauron@protonmail.com>
2024-01-03 12:14:19 +0530

    postbuild update

--- postbuild/replacerXYZ.js
+++ postbuild/replacerXYZ.js
@@ -2,7 +2,7 @@ const replace = require('replace-in-file')
 const optionsCSS = {
   files: './out/_next/static/css/*.css',
   from: [/webpack:\/\/\/mini-css-extract-plugin\/_next\//g],
-  to: ['https://pro.namesys.xyz/_next/'],
+  to: ['https://app.namesys.xyz/_next/'],
 };
 (async function () {
   try {
