cae20e67050f9c13609c1997786b6606556a9481
sshmatrix <cryptosauron@protonmail.com>
2024-01-04 09:36:22 +0530

    v1.3-beta: cleanup 2

--- components/Search.tsx
+++ components/Search.tsx
@@ -5,7 +5,74 @@ interface SearchBoxProps {
   onSearch: (query: string) => void
 }
 
-const BigSearch: React.FC<SearchBoxProps> = ({ onSearch }) => {
+export const SearchBox: React.FC<SearchBoxProps> = ({ onSearch }) => {
+  const [query, setQuery] = useState("")
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(event.target.value.toLowerCase())
+  }
+
+  const handleInputInvalid = (event: React.ChangeEvent<HTMLInputElement>) => {
+    event.target.setCustomValidity("Please enter a valid .eth name")
+  }
+
+  const handleInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    event.target.setCustomValidity("")
+  }
+
+  const handleFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    onSearch(query)
+  }
+
+  return (
+    <form 
+      className="flex-column-sans-justify"
+      onSubmit={handleFormSubmit}
+    >
+      <div
+        className="flex-row-sans-justify"
+      >
+        <input
+          type="text"
+          placeholder={"search".toLowerCase()}
+          value={query.toLowerCase()}
+          name=".eth search"
+          id="eth-search"
+          onChange={handleInputChange}
+          onInvalid={handleInputInvalid}
+          onInput={handleInput}
+          required
+          pattern=".*\.eth$"
+          title="❗ Input must end with '.eth'"
+        />
+        <button 
+          className="button"
+          style={{
+            height: '38px',
+            width: '50px',
+            marginLeft: '15px'
+          }}
+          type="submit"
+          data-tooltip='Search'
+          disabled={!query.length}
+        >
+          <span 
+            className="material-icons"
+            style={{
+              fontSize: '22px',
+              fontWeight: '700'
+            }}
+          >
+            search
+          </span>
+        </button>
+      </div>
+    </form>
+  )
+}
+
+export const BigSearch: React.FC<SearchBoxProps> = ({ onSearch }) => {
   const [query, setQuery] = useState("")
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -88,4 +155,3 @@ const BigSearch: React.FC<SearchBoxProps> = ({ onSearch }) => {
   );
 };
 
-export default BigSearch
